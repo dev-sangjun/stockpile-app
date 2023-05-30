09ccf97f523e6266ed8854e06af96440fd7d12db
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-05-30 08:59:06 -0700

    added hashed password

--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,15 +1,18 @@
 import { User } from "@prisma/client";
 import { AuthUserDto } from "../dto/auth-user.dto";
 import DBClient from "../../prisma/DBClient";
+import bcrypt from "bcryptjs";
 
 const createUser = async (authUserDto: AuthUserDto): Promise<User> => {
   const { email, username, password } = authUserDto;
+  const salt = await bcrypt.genSalt();
+  const hashedPassword = await bcrypt.hash(password, salt);
   try {
     const user = await DBClient.user.create({
       data: {
         email,
         username,
-        password,
+        password: hashedPassword,
       },
     });
     return user;
