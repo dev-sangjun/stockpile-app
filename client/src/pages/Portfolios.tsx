eaa6ff1f23744c46fcfe69d3a2275af34dfbc35a
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-05-31 10:40:09 -0700

    connected client to server & fetched portfolios

--- client/src/pages/Portfolios.tsx
+++ client/src/pages/Portfolios.tsx
@@ -1,6 +1,17 @@
-import { FC } from "react";
+import axios from "axios";
+import { FC, useEffect } from "react";
+import { DEV_SERVER_ENDPOINT, TEST_USER_ID } from "../dev/constants";
 
 const Portfolios: FC = () => {
+  useEffect(() => {
+    const fetchPortfolios = async () => {
+      const res = await axios.get(
+        `${DEV_SERVER_ENDPOINT}/portfolios?userId=${TEST_USER_ID}`
+      );
+      console.log(res.data);
+    };
+    fetchPortfolios();
+  }, []);
   return <div>Portfolios</div>;
 };
 
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { authRouter, portfolioRouter, stockRouter } from "./routes";
 import { errorHandler } from "./utils/errorHandler";
 
@@ -7,6 +8,7 @@ dotenv.config();
 
 const PORT = process.env.PORT || 5000;
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use("/auth", authRouter);
 app.use("/stocks", stockRouter);
