442fc37c33576d1ad6e10494e737f26fb8b86b31
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-05-31 10:23:19 -0700

    added bottom navbar items

--- client/src/layouts/Navbar/index.tsx
+++ client/src/layouts/Navbar/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from "react";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { NavbarItem } from "./navbar-items";
 
 interface NavbarProps {
@@ -7,17 +7,25 @@ interface NavbarProps {
 }
 
 const Navbar: FC<NavbarProps> = ({ navbarItems }) => {
+  const location = useLocation();
   const renderNavbarItems = () =>
-    navbarItems.map(navbarItem => (
-      <Link
-        key={navbarItem.label}
-        className="p-4 font-bold"
-        to={navbarItem.path}
-      >
-        {navbarItem.label}
+    navbarItems.map(({ label, path }) => (
+      <Link key={label} className="p-4 font-bold" to={path}>
+        {label}
       </Link>
     ));
-
+  const renderBottomNavbarItems = () => {
+    const isNavbarItemActive = (path: string) => path === location.pathname;
+    return navbarItems.map(({ label, path, icon }) => (
+      <Link
+        key={label}
+        className={`${isNavbarItemActive(path) ? "active" : ""}`}
+        to={path}
+      >
+        {icon}
+      </Link>
+    ));
+  };
   return (
     <div className="navbar bg-base-100 p-0 max-w-7xl mx-auto">
       <div className="flex-1">
@@ -33,6 +41,7 @@ const Navbar: FC<NavbarProps> = ({ navbarItems }) => {
           <ul className="menu menu-horizontal">{renderNavbarItems()}</ul>
         </div>
       </div>
+      <div className="btm-nav md:hidden">{renderBottomNavbarItems()}</div>
     </div>
   );
 };
