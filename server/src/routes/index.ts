d79757ea7beb24719ae31520515e78ae37261790
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-05 21:15:42 -0700

    added api routes with rootReducer

--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,13 +1,15 @@
+import { Router } from "express";
 import authRouter from "./auth.routes";
 import investmentRouter from "./investment.routes";
 import portfolioRouter from "./portfolio.routes";
 import stockRouter from "./stock.routes";
 import userRouter from "./user.routes";
 
-export {
-  authRouter,
-  investmentRouter,
-  portfolioRouter,
-  stockRouter,
-  userRouter,
-};
+const rootRouter = Router();
+rootRouter.use("/auth", authRouter);
+rootRouter.use("/investments", investmentRouter);
+rootRouter.use("/portfolios", portfolioRouter);
+rootRouter.use("/stocks", stockRouter);
+rootRouter.use("/users", userRouter);
+
+export default rootRouter;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,13 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import {
-  authRouter,
-  investmentRouter,
-  portfolioRouter,
-  stockRouter,
-  userRouter,
-} from "./routes";
+import rootRouter from "./routes";
 import { errorHandler } from "./utils/errorHandler";
 
 dotenv.config();
@@ -16,10 +10,6 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use("/auth", authRouter);
-app.use("/investments", investmentRouter);
-app.use("/portfolios", portfolioRouter);
-app.use("/stocks", stockRouter);
-app.use("/users", userRouter);
+app.use("/api", rootRouter);
 app.use(errorHandler);
 app.listen(PORT, () => console.log(`Server running at ${PORT}...`));
