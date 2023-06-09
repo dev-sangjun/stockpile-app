76447d539a4e164e4d03fad94a9a068adcb50623
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-08 23:04:49 -0700

    changed portfolios grid

--- client/src/components/EntityListGridItem/index.tsx
+++ client/src/components/EntityListGridItem/index.tsx
@@ -6,7 +6,7 @@ export interface GridItemProps {
 }
 
 const EntityListGridItem: FC<GridItemProps> = ({ title, text }) => (
-  <div className="bg-slate-100 rounded-lg p-2 flex flex-col gap-2">
+  <div className="bg-slate-100 rounded-lg p-3 flex flex-col gap-2">
     <h4 className="text-slate-500 text-sm">{title}</h4>
     <div className="font-bold">{text}</div>
   </div>
--- client/src/pages/Portfolios.tsx
+++ client/src/pages/Portfolios.tsx
@@ -4,7 +4,8 @@ import InvestmentList from "../components/InvestmentList";
 
 const Portfolios: FC = () => {
   return (
-    <div className="w-full h-full grid md:grid-cols-2 gap-4 p-4 overflow-y-auto">
+    <div className="w-full h-full grid md:grid-cols-3 gap-4 p-4 overflow-y-auto">
+      <div />
       <PortfolioList />
       <InvestmentList />
     </div>
