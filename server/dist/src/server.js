6c1166af18a70e521998262416f8f37165a4012a
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-12 22:09:17 -0700

    adding client build to server

--- server/dist/src/server.js
+++ server/dist/src/server.js
@@ -9,6 +9,7 @@ const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
 const errorHandler_1 = require("./utils/errorHandler");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
+const path_1 = __importDefault(require("path"));
 dotenv_1.default.config();
 const PORT = process.env.PORT || 5000;
 const app = (0, express_1.default)();
@@ -22,4 +23,8 @@ app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use("/api", routes_1.default);
 app.use(errorHandler_1.errorHandler);
+app.use(express_1.default.static(path_1.default.join(__dirname, "../dist/client")));
+app.get("/*", (req, res) => {
+    res.sendFile(path_1.default.join(__dirname, "../dist/client", "index.html"));
+});
 app.listen(PORT, () => console.log(`Server running at ${PORT}...`));
