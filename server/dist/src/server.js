66f4072ae351188dbd862712710ba45bd7623a90
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-12 21:27:14 -0700

    updated build

--- server/dist/src/server.js
+++ server/dist/src/server.js
@@ -9,7 +9,6 @@ const cors_1 = __importDefault(require("cors"));
 const routes_1 = __importDefault(require("./routes"));
 const errorHandler_1 = require("./utils/errorHandler");
 const cookie_parser_1 = __importDefault(require("cookie-parser"));
-const morgan_1 = __importDefault(require("morgan"));
 dotenv_1.default.config();
 const PORT = process.env.PORT || 5000;
 const app = (0, express_1.default)();
@@ -19,7 +18,6 @@ const corsOptions = {
     origin: whitelist,
 };
 app.use((0, cors_1.default)(corsOptions));
-app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
 app.use("/api", routes_1.default);
