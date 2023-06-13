8446cecbed6c1549efe77d522c70dc4d1f59ec9d
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-12 22:10:39 -0700

    adding client build to server

--- server/src/server.ts
+++ server/src/server.ts
@@ -22,6 +22,6 @@ app.use("/api", rootRouter);
 app.use(errorHandler);
 app.use(express.static(path.join(__dirname, "../dist/client")));
 app.get("/*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../dist/client", "index.html"));
+  res.sendFile(path.join(__dirname, "../client", "index.html"));
 });
 app.listen(PORT, () => console.log(`Server running at ${PORT}...`));
