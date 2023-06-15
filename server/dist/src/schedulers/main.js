2829c59c4d8593b4cc8c1c8be136e55528596644
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-15 13:25:42 -0700

    deploy to heroku

--- server/dist/src/schedulers/main.js
+++ server/dist/src/schedulers/main.js
@@ -7,13 +7,10 @@ const express_1 = __importDefault(require("express"));
 const dotenv_1 = __importDefault(require("dotenv"));
 const node_schedule_1 = __importDefault(require("node-schedule"));
 const resync_stocks_scheduler_1 = __importDefault(require("./resync-stocks.scheduler"));
-const update_user_net_worth_scheduler_1 = __importDefault(require("./update-user-net-worth.scheduler"));
 dotenv_1.default.config();
 const app = (0, express_1.default)();
 const SCHEDULER_PORT = process.env.SCHEDULER_PORT || 8080;
 app.listen(SCHEDULER_PORT, () => {
     // resync stocks every 10 minutes
-    node_schedule_1.default.scheduleJob("* * * * *", resync_stocks_scheduler_1.default);
-    // update user net worth every 5 days at 21:00 UTC (17:00 EST)
-    node_schedule_1.default.scheduleJob("0 21 * * 1-5", update_user_net_worth_scheduler_1.default);
+    node_schedule_1.default.scheduleJob("*/10 * * * *", resync_stocks_scheduler_1.default);
 });
--- server/src/schedulers/main.ts
+++ server/src/schedulers/main.ts
@@ -2,7 +2,6 @@ import express from "express";
 import dotenv from "dotenv";
 import schedule from "node-schedule";
 import resyncStocks from "./resync-stocks.scheduler";
-import updateUserNetWorth from "./update-user-net-worth.scheduler";
 
 dotenv.config();
 
@@ -10,7 +9,5 @@ const app = express();
 const SCHEDULER_PORT = process.env.SCHEDULER_PORT || 8080;
 app.listen(SCHEDULER_PORT, () => {
   // resync stocks every 10 minutes
-  schedule.scheduleJob("* * * * *", resyncStocks);
-  // update user net worth every 5 days at 21:00 UTC (17:00 EST)
-  schedule.scheduleJob("0 21 * * 1-5", updateUserNetWorth);
+  schedule.scheduleJob("*/10 * * * *", resyncStocks);
 });
