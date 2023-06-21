be0456217bed6f12718ea22b597fd4e5f67e71dc
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-20 22:04:45 -0700

    added sign out to mobile view

--- client/src/components/Navbar/BottomNavbarItems.tsx
+++ client/src/components/Navbar/BottomNavbarItems.tsx
@@ -1,9 +1,19 @@
 import { FC } from "react";
 import { Link, useLocation } from "react-router-dom";
 import navbarItems from "./navbar-items";
+import { useDispatch } from "react-redux";
+import { AppDispatch } from "../../states/store";
+import { asyncSignOut } from "../../states/user.reducer";
+import { HiOutlineArrowRightOnRectangle } from "react-icons/hi2";
 
 const BottomNavbarItems: FC = () => {
   const location = useLocation();
+  const dispatch = useDispatch<AppDispatch>();
+  const handleSignOut = () => {
+    if (confirm("Are you sure you want to sign out?")) {
+      dispatch(asyncSignOut());
+    }
+  };
   const isNavbarItemActive = (path: string) => path === location.pathname;
   const renderBottomNavbarItems = () =>
     navbarItems.map(({ label, path, icon }) => (
@@ -15,7 +25,14 @@ const BottomNavbarItems: FC = () => {
         {icon}
       </Link>
     ));
-  return <div className="btm-nav md:hidden">{renderBottomNavbarItems()}</div>;
+  return (
+    <div className="btm-nav md:hidden">
+      {renderBottomNavbarItems()}
+      <div onClick={handleSignOut}>
+        <HiOutlineArrowRightOnRectangle className="text-lg" />
+      </div>
+    </div>
+  );
 };
 
 export default BottomNavbarItems;
