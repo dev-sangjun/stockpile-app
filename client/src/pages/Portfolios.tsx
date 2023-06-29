40626e846d4c18098280172396990590a7a8fd92
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-29 13:46:39 -0700

    fixed portfolio bug

--- client/src/pages/Portfolios.tsx
+++ client/src/pages/Portfolios.tsx
@@ -5,9 +5,20 @@ import PortfolioDetails from "../components/PortfolioDetails";
 import InvestmentList from "../components/InvestmentList";
 import InvestmentDetails from "../components/InvestmentDetails";
 import { useSelectedEntity } from "../hooks";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { deselectInvestment, deselectPortfolio } from "../store/entity.reducer";
 
 const Portfolios = () => {
+  const dispatch = useDispatch();
   const { selectedPortfolio, selectedInvestment } = useSelectedEntity();
+  useEffect(() => {
+    const reset = () => {
+      dispatch(deselectPortfolio());
+      dispatch(deselectInvestment());
+    };
+    return reset;
+  }, [dispatch]);
   return (
     <div className="w-full p-2 flex flex-col md:grid md:grid-cols-2 md:grid-rows-[39rem] gap-4 overflow-hidden">
       <div
