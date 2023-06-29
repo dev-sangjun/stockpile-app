0c41e02a4819e2e6280bf3fba426cff9456d3406
dev-sangjun <33212292+dev-sangjun@users.noreply.github.com>
2023-06-29 16:59:52 -0700

    deploy to heroku

--- client/src/components/Greeting.tsx
+++ client/src/components/Greeting.tsx
@@ -4,8 +4,6 @@ import moment from "moment";
 import { RootState } from "../store";
 import { getUser } from "../store/user.reducer";
 import emojis from "../constants/emoji.constants";
-import { HiArrowPath } from "react-icons/hi2";
-import { LINK_BUTTON_CLASSES } from "../constants/classes.constants";
 
 const Greeting = () => {
   const { t } = useTranslation();
@@ -15,15 +13,10 @@ const Greeting = () => {
   }
   return (
     <div className="flex justify-between items-center">
-      <div className="flex-1 flex flex-col gap-2">
-        <div className="flex justify-between md:justify-start items-center gap-2 w-full md:w-fit max-w-2xl">
-          <h2 className="text-2xl md:text-3xl font-semibold">
-            {t("Hello", { username: userInfo.username })} {emojis.handWave}
-          </h2>
-          <button className={`${LINK_BUTTON_CLASSES.sm} text-lg`}>
-            <HiArrowPath />
-          </button>
-        </div>
+      <div className="flex flex-col gap-2">
+        <h2 className="text-2xl md:text-3xl font-semibold">
+          {t("Hello", { username: userInfo.username })} {emojis.handWave}
+        </h2>
         <p className="text-sm text-slate-500">
           {t("We'll help you reach your financial goal!")} {emojis.moneyBag}
         </p>
